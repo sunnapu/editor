dc36b1cc2cd29be56f0182eb176141383c913548
Samy Pessé <samypesse@gmail.com>
2014-04-13 19:31:35 +0200

    Improve summary design (hover and active)

--- src/views/articles.js
+++ src/views/articles.js
@@ -58,6 +58,7 @@ define([
         finish: function() {
             this.articles.appendTo(this.$(".chapter-articles"));
 
+            this.$("> .chapter-title").css("paddingLeft", (4+(this.model.get("level").length)*8)+"px");
             this.$("> .chapter-title").singleDblClick(this.open.bind(this), this.changeTitle.bind(this));
 
             return ArticleItem.__super__.finish.apply(this, arguments);
